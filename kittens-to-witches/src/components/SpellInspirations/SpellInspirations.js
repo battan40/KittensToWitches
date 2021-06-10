b2366bc34d967ac99675c03f3853c3103cb2a91e
Angela Battillo <angela.battillo@gmail.com>
2021-06-10 11:10:13 -0600

    Create initial structure for Spells

---
+++ kittens-to-witches/src/components/SpellInspirations/SpellInspirations.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './spellInspirations.css';
+
+const SpellInspirations = () => {
+  return (
+    <section className='spell-box'>
+      <p>This moments spell: I am safe;</p>
+    </section>
+  )
+}
+
+export default SpellInspirations;
