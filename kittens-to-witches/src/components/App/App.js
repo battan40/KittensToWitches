be78f1e365e19f77962c624a0da28a4a17c2d49f
Angela Battillo <angela.battillo@gmail.com>
2021-06-10 21:59:12 -0600

    Create appropriate life cycle method for fetches

--- kittens-to-witches/src/components/App/App.js
+++ kittens-to-witches/src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SpellInspirations from '../SpellInspirations/SpellInspirations';
+import { fetchKittens, fetchSwansonQuotes } from '../../utils/apiCalls'
 import background from '../../assets/enchantedForest.png'
 import './App.css';
 
@@ -11,11 +12,28 @@ class App extends Component{
         error: ''
       }
   }
+
+  addSpell = (newSpell) => {
+    this.setState({ spells: [...this.state.spells, newSpell]})
+  }
+
+  componentDidMount = async () => {
+    const swanspiration = await fetchSwansonQuotes()
+      .then(data => data[0])
+    const kittenFamiliar = await fetchKittens()
+      .then(data => data.file)
+    const organizeKitsNQuotes = {
+      id: 0, swanspiration, kittenFamiliar,
+    }
+    this.setState({ spells: [...this.state.spells, organizeKitsNQuotes]})
+  }
+
   render() {
+
     return (
       <main className='App' style={{ backgroundImage: `url(${background})`}}>
         <h1 className='app-title'>Kittens To Witches</h1>
-        <SpellInspirations />
+        <SpellInspirations spells={this.state.spells}/>
       </main>
     );
   }
