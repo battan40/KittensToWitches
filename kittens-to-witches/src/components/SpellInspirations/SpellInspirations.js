73b9282c9f9b3c8ccf465b7a6dfe81da2e9b369b
Angela Battillo <angela.battillo@gmail.com>
2021-06-10 22:12:41 -0600

    Adjust structure of function component to include all intended aesthetic

--- kittens-to-witches/src/components/SpellInspirations/SpellInspirations.js
+++ kittens-to-witches/src/components/SpellInspirations/SpellInspirations.js
@@ -2,11 +2,25 @@ import React from 'react';
 import SpellCard from '../SpellCard/SpellCard'
 import './spellInspirations.css';
 
-const SpellInspirations = () => {
-  return (
-    <section className='spell-box'>
-      <SpellCard />
-    </section>
+const SpellInspirations = ({ spells }) => {
+
+  const cardDisplay = spells.map(spell => {
+    return (
+        <SpellCard
+         key={spell.id}
+         id={spell.id}
+         kittenFamiliar={spell.kittenFamiliar}
+         swanspiration={spell.swanspiration}
+         keep={spell.keep}
+        />
+      )
+
+  })
+
+  return(
+    <article className='spell-box'>
+      { cardDisplay }
+    </article>
   )
 }
 
