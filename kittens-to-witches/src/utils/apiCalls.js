8d50cb6d76d368e8450b69dbf6c2f8dfeb6d576c
Angela Battillo <angela.battillo@gmail.com>
2021-06-10 22:33:50 -0600

    Place swanspiration quote fetches

--- kittens-to-witches/src/utils/apiCalls.js
+++ kittens-to-witches/src/utils/apiCalls.js
@@ -5,3 +5,11 @@ export const fetchKittens = () => {
 
     .catch(err => 'Whoops looks like your kitten familiar is out playing today.  Lets try this again later')
 }
+
+export const fetchSwansonQuotes = () => {
+  return fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+    .then(response => response.json())
+    .then(data => data)
+
+    .catch(err => 'This swanspiration is experiencing delays.  Please try again later')
+}
