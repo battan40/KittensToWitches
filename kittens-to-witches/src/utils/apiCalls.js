02feeecb7edef86a91e5994983e0c7f5686a0801
Angela Battillo <angela.battillo@gmail.com>
2021-06-10 22:32:30 -0600

    Make kitten famliar image grabber

--- kittens-to-witches/src/utils/apiCalls.js
+++ kittens-to-witches/src/utils/apiCalls.js
@@ -1,1 +1,7 @@
+export const fetchKittens = () => {
+  return fetch('https://aws.random.cat/meow')
+    .then(response => response.json())
+    .then(data => data)
 
+    .catch(err => 'Whoops looks like your kitten familiar is out playing today.  Lets try this again later')
+}
