3cac44571361cf3c253d9bbbd6e09a365da5212d
Angela Battillo <angela.battillo@gmail.com>
2021-06-12 16:09:10 -0600

    Make background image test

--- kittens-to-witches/cypress/integration/kittensToWitches.js
+++ kittens-to-witches/cypress/integration/kittensToWitches.js
@@ -16,12 +16,12 @@ describe('Show main page of Kittens To Witches', () => {
     cy.url().should('eq', 'http://localhost:3000/')
   })
 
-  it('Should display title on the main page display', () => {
-    cy.get('.app-title').should('contain', 'Kittens')
+  it('Should be show a background image covering the whole page', () => {
+    cy.get('main').should('have.css', 'background-image', 'url("http://localhost:3000/static/media/enchantedForest.3b87ed50.png")')
   })
 
-  it('Should display loading message while swanspiration loads', () => {
-    cy.get('.loading').should('contain', 'Loading...')
+  it('Should display title on the main page display', () => {
+    cy.get('.app-title').should('contain', 'Kittens')
   })
 
   it('Should load display for quote and image upon page load', () => {
@@ -29,6 +29,8 @@ describe('Show main page of Kittens To Witches', () => {
   })
 
   it('Should display ron quote', () => {
-    cy.get('ronspiration').should('be.visible')
+    cy.get('.ronspiration').should('contain', 'I')
   })
+
+
 })
