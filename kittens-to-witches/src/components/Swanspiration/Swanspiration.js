f2d4ff460f4845e597212fa0b0c2be020c6bfd59
Angela Battillo <angela.battillo@gmail.com>
2021-06-12 12:58:15 -0600

    Change spacing for dev empathy

--- kittens-to-witches/src/components/Swanspiration/Swanspiration.js
+++ kittens-to-witches/src/components/Swanspiration/Swanspiration.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './swanspiration.css'
 
-const Swanspiration = ({ swanspiration }) => {
+const Swanspiration = ({ swanspiration, loading }) => {
   return(
     <article className='kit-card'>
-      <img className='cat-pic' src={swanspiration.kittenFamiliar} alt='Adorable kitten familiar'/>
+      <img className='cat-pic'
+         src={swanspiration.kittenFamiliar} alt='Adorable kitten familiar'
+         />
       <p className='ronspiration'>{swanspiration.swanspiration}</p>
     </article>
   )
