06fbf982b6e0d5eff3b98b1702c1e59858337e9c
Angela Battillo <angela.battillo@gmail.com>
2021-06-13 18:17:36 -0600

    Establish presence of content on about page testing

--- kittens-to-witches/cypress/integration/about.js
+++ kittens-to-witches/cypress/integration/about.js
@@ -8,7 +8,11 @@ describe('Show about page on route', () => {
     cy.url().should('eq', 'http://localhost:3000/about')
   });
 
-
+  it('Should open to a specific font', () => {
+    cy.url().should('eq', 'http://localhost:3000/about')
+      .get('.about-box').should('be.visible')
+      .get('.about-box').should('have.css', 'font-family')
+  })
 
 
 
