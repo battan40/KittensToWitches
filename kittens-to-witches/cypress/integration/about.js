4c2e5ec4dbef774ddb2c360ad4af5618c06eddb6
Angela Battillo <angela.battillo@gmail.com>
2021-06-13 18:16:56 -0600

    Create initial describe block

---
+++ kittens-to-witches/cypress/integration/about.js
@@ -0,0 +1,18 @@
+describe('Show about page on route', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/about')
+  })
+
+  it('Should be able to open to the main page', () => {
+    cy.url().should('eq', 'http://localhost:3000/about')
+  });
+
+
+
+
+
+
+
+
+})
