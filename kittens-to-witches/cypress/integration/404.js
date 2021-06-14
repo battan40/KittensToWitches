835e0b73ec5aa5b8ec3ebb80d403f9f328dadc0a
Angela Battillo <angela.battillo@gmail.com>
2021-06-13 18:58:09 -0600

    Add visibility check for 404 page

--- kittens-to-witches/cypress/integration/404.js
+++ kittens-to-witches/cypress/integration/404.js
@@ -4,8 +4,15 @@ describe('Show about page on route', () => {
     cy.visit('http://localhost:3000/about')
   });
 
-  it('Should be able to open to the main page', () => {
-    cy.url().should('eq', 'http://localhost:3000/about')
+  it('Should be able to open to the 404 error page', () => {
+    cy.url().should('eq', 'http://localhost:3000/404')
   });
-  
+
+  it('Should find a message on the 404 page', () => {
+    cy.get('.error-page').should('be.visible')
+      .get('.error-page').should('contain', '404')
+  })
+
+
+
 }
