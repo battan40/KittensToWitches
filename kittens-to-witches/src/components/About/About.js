79cdc9bcd4b278956a1c6c2ca57080641e2f0dc2
Angela Battillo <angela.battillo@gmail.com>
2021-06-13 21:40:47 -0600

    Add Method to allow in favorites component render

--- kittens-to-witches/src/components/About/About.js
+++ kittens-to-witches/src/components/About/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './about.css'
 
 const About = () => {
@@ -8,7 +9,11 @@ const About = () => {
 
       <p className='about-body'>Sometimes inspiration comes from the strangest of places.  This app was created with the intention of offering the ridiculous wisdom and folly of Ron Swanson.  Then it only seemed right to send Ron Swanson messages with a kitten familiar.  The hope is that you feel at home, laugh and then feel inspired to cast a spell.
       </p>
+      <Link to='/'>
+      <button className='error-home'>Home</button>
+      </Link>
     </section>
+
   )
 }
 
