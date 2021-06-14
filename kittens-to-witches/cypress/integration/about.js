24ab0b9b2a96a4512fe19ac3995b77fdd00537bd
Angela Battillo <angela.battillo@gmail.com>
2021-06-13 18:22:05 -0600

    Add semi-colons and remove space for continuity

--- kittens-to-witches/cypress/integration/about.js
+++ kittens-to-witches/cypress/integration/about.js
@@ -2,7 +2,7 @@ describe('Show about page on route', () => {
 
   beforeEach(() => {
     cy.visit('http://localhost:3000/about')
-  })
+  });
 
   it('Should be able to open to the main page', () => {
     cy.url().should('eq', 'http://localhost:3000/about')
@@ -12,23 +12,24 @@ describe('Show about page on route', () => {
     cy.url().should('eq', 'http://localhost:3000/about')
       .get('.about-box').should('be.visible')
       .get('.about-box').should('have.css', 'font-family')
-  })
+  });
 
   it('Should open to a title page', () => {
     cy.url().should('eq', 'http://localhost:3000/about')
       .get('.about-title').should('be.visible')
       .get('.about-title').should('contain', 'Why')
-  })
+  });
 
   it('Should have a specific color for ease of viewing', () => {
     cy.url().should('eq', 'http://localhost:3000/about')
       .get('.about-title').should('be.visible')
       .get('.about-title').should('have.css', 'font-size')
-  })
+  });
 
   it('Should open to a title page', () => {
     cy.url().should('eq', 'http://localhost:3000/about')
       .get('.about-body').should('be.visible')
       .get('.about-body').should('contain', 'Sometimes')
-  })
-})
+  });
+
+});
