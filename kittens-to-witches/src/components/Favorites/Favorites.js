d412020712d724a8ce92554fffd335afe69531b6
Angela Battillo <angela.battillo@gmail.com>
2021-06-13 22:00:19 -0600

    Create favorites component

---
+++ kittens-to-witches/src/components/Favorites/Favorites.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SpellCard from '../SpellCard/SpellCard'
+import { Link } from 'react-router-dom'
+import './favorites.css'
+
+const Favorites = ( { spells, favoriteSpell }) => {
+
+
+
+}
+
+export default Favorites;
