eaf563999bfb8e8e32dd7d788fef7e613b0ddef9
Angela Battillo <angela.battillo@gmail.com>
2021-06-13 22:03:51 -0600

    Add the spells display pathway to create favoriting

--- kittens-to-witches/src/components/Favorites/Favorites.js
+++ kittens-to-witches/src/components/Favorites/Favorites.js
@@ -5,7 +5,27 @@ import './favorites.css'
 
 const Favorites = ( { spells, favoriteSpell }) => {
 
+  const spellsDisplay = spells.map(spell => {
+    return (
+        <SpellCard
+         key={spell.id}
+         id={spell.id}
+         title={spell.title}
+         description={spell.description}
+         keep={spell.keep}
+         favoriteSpell={favoriteSpell}
+        />
+      )
+  })
 
+  return(
+    <article className='spell-box'>
+      { spellsDisplay }
+      <Link to='/'>
+      <button className='error-home'>Home</button>
+      </Link>
+    </article>
+  )
 
 }
 
