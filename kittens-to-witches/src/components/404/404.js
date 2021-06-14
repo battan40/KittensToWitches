25b32634610ffb582e8579c53df92badd03548c7
Angela Battillo <angela.battillo@gmail.com>
2021-06-13 21:48:45 -0600

    Create link pathway for home button

--- kittens-to-witches/src/components/404/404.js
+++ kittens-to-witches/src/components/404/404.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './404.css'
 
 const FourOFour = () => {
   return(
     <section>
       <h1 className='error-page'>404 Page Not Found</h1>
-      <button className='error-home' onClick={() => goHome()}>Home</button>
+      <Link to='/'>
+      <button className='error-home'>Home</button>
+      </Link>
     </section>
   )
 }
